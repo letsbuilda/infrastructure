458a899ae75c1c73ce8fd051790416eaafad0268
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-02-20 20:51:23 -0600

    add sql script to create db roles
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

---
+++ infrastructure/discord-bots/init.sql
@@ -0,0 +1,5 @@
+CREATE USER alais WITH PASSWORD 'changemeplz';
+CREATE DATABASE alais WITH OWNER alais;
+
+CREATE USER larkbot WITH PASSWORD 'changemeplz';
+CREATE DATABASE larkbot WITH OWNER larkbot;
