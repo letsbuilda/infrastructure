4eb3e74e2db8270843039ab5e49f54eee1d223ed
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-03-25 16:44:29 -0500

    add alessia to discord bots
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- infrastructure/discord-bots/init.sql
+++ infrastructure/discord-bots/init.sql
@@ -3,3 +3,6 @@ CREATE DATABASE alais WITH OWNER alais;
 
 CREATE USER larkbot WITH PASSWORD 'changemeplz';
 CREATE DATABASE larkbot WITH OWNER larkbot;
+
+CREATE USER alessia WITH PASSWORD 'changemeplz';
+CREATE DATABASE alessia WITH OWNER alessia;
