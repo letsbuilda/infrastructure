626c1081d457df9b901959e2ae2b27b89c2efbe9
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-10-16 12:08:55 -0500

    Delete infrastructure/gretchen/init.sql (#11)
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- infrastructure/gretchen/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER alais WITH PASSWORD 'changemeplz';
-CREATE DATABASE alais WITH OWNER alais;
-
-CREATE USER larkbot WITH PASSWORD 'changemeplz';
-CREATE DATABASE larkbot WITH OWNER larkbot;
-
-CREATE USER alessia WITH PASSWORD 'changemeplz';
-CREATE DATABASE alessia WITH OWNER alessia;
